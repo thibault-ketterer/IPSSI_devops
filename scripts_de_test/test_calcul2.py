1a491787ff977deb59be900c0e62e2553818c55b
thibault ketterer <thekit@gmail.com>
2019-11-09 00:13:47 +0100

    script de test

---
+++ scripts_de_test/test_calcul2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from calcul import division
+from calcul import multiplication
+
+print(division(8, 2))
+print(division(8, 0))
+print(multiplication(8, 0))
+print(multiplication(8, 5))
